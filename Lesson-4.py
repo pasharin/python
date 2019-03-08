655e05fd9d08123073e377e45a028eabb2fbf62c
pasharin <asharinpavel@gmail.com>
2019-03-08 11:25:22 +0500

    ДЗ 4-6. Не до конца.

---
+++ Lesson-4.py
@@ -0,0 +1,217 @@
+# Все задачи текущего блока решите с помощью генераторов списков!
+
+# Задание-1:
+# Дан список, заполненный произвольными целыми числами. 
+# Получить новый список, элементы которого будут
+# квадратами элементов исходного списка
+# [1, 2, 4, 0] --> [1, 4, 16, 0]
+
+my_list = [1, 2, 4, 0]
+sq_list = [a ** 2 for a in my_list]
+print(sq_list)
+
+# Задание-2:
+# Даны два списка фруктов.
+# Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruit_list = ['банан', 'апельсин', 'яблоко', 'груша']
+another_list = ['киви', 'банан', 'персик', 'яблоко']
+sort_list = list (set (fruit_list) & set (another_list))
+print(sort_list)
+
+# Задание-3:
+# Дан список, заполненный произвольными числами.
+# Получить список из элементов исходного, удовлетворяющих следующим условиям:
+# + Элемент кратен 3
+# + Элемент положительный
+# + Элемент не кратен 4
+
+my_list = [1, 2, 4, 9, 0, -3, 16, -23, -9]
+sort_list = [el for el in my_list if el % 3 == 0 and el >=0 and el % 4 !=0]
+print(sort_list)
+
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+
+import re
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+line_str = re.findall(r'[a-z]+', line)
+print('Символы в нижнем регистре с использованием модуля re: \n',line_str)
+
+symbol = list(map(lambda x: chr(x), list(range(65, 91))))  # Преобразуем список из кодов ANSI в список букв A-Z
+line_new = list(line)
+
+for i, element in enumerate(line_new[:]):
+       for element_2 in symbol:
+              if element == element_2:
+                     line_new[i] = ' '
+
+# Соединение списка в строку методом .join и разбиение строки по символу ' '
+stroka = ''.join(line_new).split(' ')
+
+line_str_2 = [i for i in stroka if i != '']
+print('Символы в нижнем регистре без использования модуля re: \n', line_str_2)
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+line_2_str = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print('Список с использованием модуля re: \n', line_2_str)
+
+# 2. Решение без re
+symbol1 = list(map(lambda x: chr(x), list(range(65, 91))))  # Преобразуем список из кодов ANSI в список букв A-Z
+symbol2 = list(map(lambda x: chr(x), list(range(97, 123))))  # Преобразуем список из кодов ANSI в список букв a-z
+line_new = list(line_2)
+
+lst = []
+i = len(line_new) - 1
+# Находим  символ в верхнем регистре после которого стоят еще два символа в верхнем регистре
+while i >= 0:
+       if line_new[i] in symbol2:
+              lst.append(line_new[i])
+       elif line_new[i] in symbol1 and i <= len(line_new) - 3 and line_new[i + 1] in symbol1 and line_new[
+              i + 2] in symbol1:
+              lst.append(line_new[i])
+       else:
+              lst.append(' ')
+       i -= 1
+lst.reverse()  # Переворачиваем список
+
+i = 0
+lst2 = []
+registr = True  # Начальное условие поиска сортировки символов в верхнем регистре
+# Фильтрация списка.
+while i <= len(lst) - 1:
+       if lst[i] in symbol2:
+              registr = True
+       if lst[i] in symbol1 and lst[i - 1] in symbol2 and lst[i - 2] in symbol2:
+              lst2.append(lst[i])
+              registr = False
+       elif lst[i] in symbol1 and registr == False:
+              lst2.append(lst[i])
+       else:
+              lst2.append(' ')
+       i += 1
+stroka = ''.join(lst2).split(' ')  # Преобразование в строку и разбиение по пробелам
+
+line_str_3 = [i for i in stroka if i != '']  # Формирование выхрдного списка из строки
+print('Список без использованием модуля re: \n', line_str_3)
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+import random
+
+MAX_CHISLO = 2500
+
+# spisok = ''.join([random.randint(0,9) for _ in range(10)])
+spisok = [random.randint(0, 9) for _ in range(MAX_CHISLO)]
+spisok = ''.join(list(map(lambda x: str(x), spisok)))
+print(spisok)
+# Запись в файл
+path = 'data\\' + 'script' + '.txt'
+with open(path, 'w', encoding='UTF-8') as file:
+       file.write(str(spisok))
+
+# Чтение файла
+with open(path, 'r', encoding='UTF-8') as file:
+       stroka_1 = list(file.read())
+print(stroka_1)
+
+
+
+# Задание-1:
+# Матрицы в питоне реализуются в виде вложенных списков:
+# Пример. Дано:
+matrix = [[1, 0, 8],
+          [3, 4, 1],
+          [0, 4, 2]]
+          
+# Выполнить поворот (транспонирование) матрицы
+# Пример. Результат:
+# matrix_rotate = [[1, 3, 0],
+#                  [0, 4, 4],
+#                  [8, 1, 2]]
+
+# Суть сложности hard: Решите задачу в одну строку
+
+# Задание-2:
+# Найдите наибольшее произведение пяти последовательных цифр в 1000-значном числе.
+# Выведите произведение и индекс смещения первого числа последовательных 5-ти цифр.
+# Пример 1000-значного числа:
+number = """
+73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450"""
+
+
+# Задание-3 (Ферзи):
+# Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били
+# друг друга. Вам дана расстановка 8 ферзей на доске.
+# Определите, есть ли среди них пара бьющих друг друга.
+# Программа получает на вход восемь пар чисел,
+# каждое число от 1 до 8 — координаты 8 ферзей.
+# Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
+
+
